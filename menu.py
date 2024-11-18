8bd6a8d8eeef6a155a6871eea6bc92887cfd43f4
Amanda Lemos Ribas <amandaribas@id.uff.br>
2024-11-18 20:18:17 -0300

    .

--- menu.py
+++ menu.py
@@ -1,5 +1,16 @@
 from PPlay.window import *
 from PPlay.gameimage import *
+from PPlay.sprite import *
+
+janela = Window(1024,682)
+background = GameImage("images/background_prisao.png")
+personagem1 = Sprite("images/deolene.png")
+personagem2 = Sprite("images/guilherme.png")
 
 while True:
     
+    personagem1.set_position(200, 300)
+    background.draw()
+    personagem1.draw()
+    #personagem2.draw()
+    janela.update()
