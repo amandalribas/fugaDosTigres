ac8f92261e78f96aa916dd3a0dbe786158e01dda
Amanda Lemos Ribas <amandaribas@id.uff.br>
2024-11-18 20:29:24 -0300

    .

--- menu.py
+++ menu.py
@@ -2,15 +2,24 @@ from PPlay.window import *
 from PPlay.gameimage import *
 from PPlay.sprite import *
 
+def posInicialPersonagem(sprite):
+    sprite.set_position(100,470)
+
+
 janela = Window(1024,682)
 background = GameImage("images/background_prisao.png")
 personagem1 = Sprite("images/deolene.png")
 personagem2 = Sprite("images/guilherme.png")
+janela.set_title("FASE 1: PRISÃO")
+
+
 
 while True:
     
-    personagem1.set_position(200, 300)
+    posInicialPersonagem(personagem1)
     background.draw()
     personagem1.draw()
+
+
     #personagem2.draw()
     janela.update()
