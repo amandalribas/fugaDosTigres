29834569096701bc6149798efba3446cdd4a85c0
Amanda Lemos Ribas <amandaribas@id.uff.br>
2025-01-09 14:23:43 -0300

    .

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 contVidas = 3
 velBackground = 550
-velPulo = 600
+velPulo = 550
 pontos = 0
 personagemEscolhido = "assets/sprites/guilherme.png"
 personagemAndando = "assets/sprites/guilhermerun.png"
